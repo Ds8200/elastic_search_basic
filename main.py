e21276ad5bb4de6d1133524d9d34ae3f4d22b8e2
Ds8200 <ds8200h@gmail.com>
2026-02-22 17:35:21 +0200

    ferfect_codes

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-from src.elastic.dal import create_edge_ngram_analyzer, insert_doc, search_by_name
+from src.elastic.dal import create_products_index, insert_many_docs, refresh_index, search_by_name
 from src.file.json import read_json_file
 
 path_data_products = "data/products.json"
 index_name = "products"
 
 def main():
-    # create_edge_ngram_analyzer(index_name)
+    create_products_index(index_name)
 
-    # products = read_json_file(path_data_products)
+    products = read_json_file(path_data_products)
 
-    # for i, product in enumerate(products, start=1):
-    #     insert_doc(index_name, i, product)
+    insert_many_docs(index_name, products)
+    
+    refresh_index(index_name)
 
     while True:
         name = input("Enter product name to search (or 'exit' to quit): ")
--- src/elastic/dal.py
+++ src/elastic/dal.py
@@ -1,50 +1,48 @@
-from src.elastic.connect import es
+from pathlib import Path
 
-def create_edge_ngram_analyzer(index_name: str):
-    index_body = {
-        "settings": {
-            "analysis": {
-                "tokenizer": {
-                    "autocomplete_tokenizer": {
-                        "type": "edge_ngram",
-                        "min_gram": 2,
-                        "max_gram": 20,
-                        "token_chars": ["letter", "digit"]
-                    }
-                },
-                "analyzer": {
-                    "autocomplete_analyzer": {
-                        "type": "custom",
-                        "tokenizer": "autocomplete_tokenizer",
-                        "filter": ["lowercase"]
-                    },
-                    "autocomplete_search_analyzer": {
-                        "type": "custom",
-                        "tokenizer": "standard",
-                        "filter": ["lowercase"]
-                    }
-                }
-            }
-        },
-        "mappings": {
-            "properties": {
-                "name": {
-                    "type": "text",
-                    "analyzer": "autocomplete_analyzer",
-                    "search_analyzer": "autocomplete_search_analyzer"
-                }
-            }
-        }
-    }
+from src.file.json import read_json_file
+from src.elastic.connect import es
+from elasticsearch.helpers import bulk
+
+current_path = Path(__file__).parent.parent.parent
+path_data_config =  current_path / "data" / "config_elastic.json"
+
+
+def create_products_index(index_name: str):
+    index_body = read_json_file(path_data_config)
 
     if not es.indices.exists(index=index_name):
         es.indices.create(index=index_name, body=index_body)
+        print(f"Created index '{index_name}' with edge_ngram analyzer 👍")
+    else:
+        print(f"Index '{index_name}' already exists 👍")
     
-    print(f"Created index '{index_name}' with edge_ngram analyzer👍")
-    
+
 def insert_doc(index_name: str, id: int, doc: dict):
     es.index(index=index_name, id=id, document=doc)
     print("Inserted doc👍")
+    
+
+def insert_many_docs(index_name: str, docs: list[dict]):
+    actions = [
+        {
+            "_index": index_name,
+            "_id": idx + 1,
+            "_source": product
+        }
+        for idx, product in enumerate(docs)
+    ]
+
+    success, errors = bulk(es, actions)
+
+    print("Inserted:", success)
+    print("Errors:", errors)
+
+
+def refresh_index(index_name: str):
+    es.indices.refresh(index=index_name)
+    print("Refreshed index👍")
+
 
 def search_by_name(index_name: str, name: str) -> list[dict]:
     query={
